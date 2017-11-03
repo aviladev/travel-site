3694af57d5bfe977225f63815a4db5233e7c9bd4
Pedro Ávila <pedroavila95@gmail.com>
2017-11-03 19:56:51 -0200

    Completed large hero block and intro content block

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssvars = require('postcss-simple-vars')
+const nested = require('postcss-nested')
+const cssImport = require('postcss-import')
+const mixins = require('postcss-mixins')
+
+gulp.task('styles', (done) =>
+  gulp.src('./app/assets/styles/styles.css')
+    .pipe(
+      postcss(
+        [
+          cssImport,
+          mixins,
+          cssvars,
+          nested,
+          autoprefixer
+        ]
+    ))
+    .on('error', done)
+    .pipe(
+      gulp.dest('./app/temp/styles')
+    )
+)
